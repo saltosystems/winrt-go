b90f3ab5ea03c54639588748bf46a31cb0d0166b
Jagoba Gascón <jagoba@arima.eu>
2022-06-08 12:43:49 +0200

    codegen: add static and factory method skipping
    this is an easy way of removing some methods from the generated code
    that may be failing. This may require a more sophisticated aproach in
    the future to skip methods by name, but it is good enough for now

--- winrt/winrt.go
+++ winrt/winrt.go
@@ -2,7 +2,7 @@ package winrt
 
 import "github.com/go-ole/go-ole"
 
-//go:generate go run github.com/saltosystems/winrt-go/cmd/winrt-go-gen -debug -class Windows.Storage.Streams.Buffer
+//go:generate go run github.com/saltosystems/winrt-go/cmd/winrt-go-gen -debug -class Windows.Storage.Streams.Buffer -skip-statics
 
 func makeError(hr uintptr) error {
 	if hr != 0 {
